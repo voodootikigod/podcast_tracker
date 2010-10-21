7fa692ec2194df6a84b5c88ec7cdc3499c976df0
Chris Williams <chris@iterativedesigns.com>
2010-10-20 22:48:15 -0400

    dynamic directory

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-exports = {
+module.exports = {
   port: 8080,
   directory: "./files",
+  redirect: "http://www.aminutewithbrendan.com",
   headers: {amwb: "A Minute With Brendan", jsconflive: "JSConf Live"}
 }
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var nodestatic = require('node-static');
 var sys = require("sys");
 var redis = require("redis");
-var config = require("./config")
+var config = require("./config");
+var http = require("http");
+
+
 
 var fileServer = new nodestatic.Server(config.directory);
-
+sys.p(fileServer)
 client = redis.createClient();
 
 require('http').createServer(function (request, response) {
   request.addListener('end', function () {
-    if (request.url == "/statistics.json")  {
+    sys.puts(request.url);
+    if (request.url == "/") {
+      response.writeHead(301, {"Location": config.redirect});
+      response.end();
+    } else if (request.url == "/statistics.json")  {
       client.keys("podcast:*", function (err, res) {
         var length = res.length, count = 0, results = {};
         res.forEach(function (item) { 
@@ -29,12 +36,14 @@ require('http').createServer(function (request, response) {
     } else {
       fileServer.serve(request, response, function (err, results) {
         if (err && (err.status === 404)) { // If the file wasn't found
-          response.writeHead(err.status, err.headers);
+          sys.puts("404 -> redirected")
+          response.writeHead(301, {"Location": config.redirect});
           response.end();
         } else { 
+          file_parts = request.url.split("/");
+          filename = file_parts[file_parts.length-1];
+          sys.puts("sending file: "+filename);
           if (/(mp3|ogg)$/.exec(request.url)) {
-            file_parts = request.url.split("/");
-            filename = file_parts[file_parts.length-1];
             client.incr("podcast:"+filename);
             client.incr("podcast:"+filename.split(".")[0]);
           }
