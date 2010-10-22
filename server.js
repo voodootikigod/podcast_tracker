7d2ecaf17781d21db234b644304ec5202d22682a
Chris Williams <chris@iterativedesigns.com>
2010-10-21 20:53:34 -0400

    Update.

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ var nodestatic = require('node-static');
 var sys = require("sys");
 var redis = require("redis");
 var config = require("./config");
+var jade = require("jade");
 var http = require("http");
 
 
 
 var fileServer = new nodestatic.Server(config.directory);
-sys.p(fileServer)
 client = redis.createClient();
 
 require('http').createServer(function (request, response) {
   request.addListener('end', function () {
-    sys.puts(request.url);
-    if (request.url == "/") {
+    var url = request.url;
+    if (url == "/") {
       response.writeHead(301, {"Location": config.redirect});
       response.end();
-    } else if (request.url == "/statistics.json")  {
+    } else if (url == "/statistics")  {
       client.keys("podcast:*", function (err, res) {
         var length = res.length, count = 0, results = {};
         res.forEach(function (item) { 
@@ -24,11 +24,11 @@ require('http').createServer(function (request, response) {
             results[item] = parseInt(resp.toString('utf8'));
             count += 1
             if (count == length) { 
-              response.writeHead(200, {"Content-Type": "application/json"})
-              response.end(JSON.stringify({
-                headers: config.headers,
-                data:    results
-              }));
+              
+              jade.renderFile('index.jade', { headers: config.headers }, function(err, html){
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.end(html);
+              });
             }
           });
         })
@@ -36,14 +36,12 @@ require('http').createServer(function (request, response) {
     } else {
       fileServer.serve(request, response, function (err, results) {
         if (err && (err.status === 404)) { // If the file wasn't found
-          sys.puts("404 -> redirected")
           response.writeHead(301, {"Location": config.redirect});
           response.end();
         } else { 
-          file_parts = request.url.split("/");
+          file_parts = url.split("/");
           filename = file_parts[file_parts.length-1];
-          sys.puts("sending file: "+filename);
-          if (/(mp3|ogg)$/.exec(request.url)) {
+          if (/(mp3|ogg)$/.exec(url)) {
             client.incr("podcast:"+filename);
             client.incr("podcast:"+filename.split(".")[0]);
           }
