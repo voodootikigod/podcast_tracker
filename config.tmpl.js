ec048995a833aa3969a62f15d06aa9643fabad4c
Chris Williams <chris@iterativedesigns.com>
2010-10-30 21:19:48 -0400

    even better url handling.

--- config.tmpl.js
+++ config.tmpl.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 8080,
   // location: "files",
-  statistics_uri: "/admin/index",
+  statistics_uri: "/admin",
   reset_uri: "/admin/reset",
   location: "http://aminutewith.s3.amazonaws.com",
   redirect: "http://www.aminutewithbrendan.com",
