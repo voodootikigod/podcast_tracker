fe4d50a89be05e75891db98db305320fcb8235b7
Chris Williams <chris@iterativedesigns.com>
2010-10-30 20:23:17 -0400

    Easier to nginx basic authing.

--- config.tmpl.js
+++ config.tmpl.js
@@ -1,8 +1,8 @@
 module.exports = {
   port: 8080,
   // location: "files",
-  statistics_uri: "/statistics",
-  reset_uri: "/reset",
+  statistics_uri: "/admin/index",
+  reset_uri: "/admin/reset",
   location: "http://aminutewith.s3.amazonaws.com",
   redirect: "http://www.aminutewithbrendan.com",
   headers: {amwb: "A Minute With Brendan", jsconflive: "JSConf Live"}
